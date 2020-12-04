b439ed07ac16a520977b1d89616a87a9a92792eb
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-12-04 08:39:01 -0600

    [test] testing

--- routes/airquality.js
+++ routes/airquality.js
@@ -9,8 +9,9 @@ airRouter.get('/airquality', (req, res) => {
 
 airRouter.get('/airquality/location', (req, res) => {
     const citystate = req.query.location;
-    console.log(citystate);
-    axios.get(`https://api.weatherbit.io/v2.0/current/airquality?city=${citystate}&key=${process.env.AIR_API_KEY}`)
+    const state = req.query.state;
+    // console.log(citystate);
+    axios.get(`https://api.weatherbit.io/v2.0/current/airquality?city=${citystate},${state}&key=${process.env.AIR_API_KEY}`)
     .then((response) => {
         const air = response.data;
         // console.log(air);
@@ -22,15 +23,19 @@ airRouter.get('/airquality/location', (req, res) => {
 
 airRouter.post('/profile', (req, res) => {
     const { city_name } = req.body;
-    console.log(city_name);
-    axios.get(`https://api.weatherbit.io/v2.0/current/airquality?city=${city_name}&key=${process.env.AIR_API_KEY}`)
+    const state = req.body.state_code;
+    // console.log(city_name);
+    // console.log(state);
+    console.log(req.body.state_code);
+    axios.get(`https://api.weatherbit.io/v2.0/current/airquality?city=${city_name},${state}&key=${process.env.AIR_API_KEY}`)
     .then((response) => {
         const air = response.data;
-        console.log('unique', air);
+        console.log('unique', response);
     db.city.create({
-        name: city_name
+        name: city_name,
+        state: state
     }).then(() => {
-        res.render('airquality', { query: air })
+        res.redirect('/profile')
         })
     }).catch((error) => {
         console.log(error);
@@ -48,7 +53,35 @@ module.exports = airRouter;
     
     
 
-    
+// airRouter.get('/airquality/location', (req, res) => {
+//     const citystate = req.query.location;
+//     console.log(citystate);
+//     axios.get(`https://api.weatherbit.io/v2.0/current/airquality?city=${citystate}&key=${process.env.AIR_API_KEY}`)
+//     .then((response) => {
+//         const air = response.data;
+//         // console.log(air);
+//         res.render('airquality', { query: air })
+//     }).catch((error) => {
+//             console.log(error);
+//          });
+// });
+
+// airRouter.post('/profile', (req, res) => {
+//     const { city_name } = req.body;
+//     console.log(city_name);
+//     axios.get(`https://api.weatherbit.io/v2.0/current/airquality?city=${city_name}&key=${process.env.AIR_API_KEY}`)
+//     .then((response) => {
+//         const air = response.data;
+//         console.log('unique', air);
+//     db.city.create({
+//         name: city_name
+//     }).then(() => {
+//         res.render('airquality', { query: air })
+//         })
+//     }).catch((error) => {
+//         console.log(error);
+//     })
+// })
     
     
     
