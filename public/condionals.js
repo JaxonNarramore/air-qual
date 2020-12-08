197d6a004ae51ce67edabb512eba2fa7235f96ce
Jaxon Narramore <jaxonnarramore@Jaxons-MBP.attlocal.net>
2020-12-07 19:25:11 -0600

    [style.css] Add styling to pages

---
+++ public/condionals.js
@@ -0,0 +1,13 @@
+if (document.getElementById('li2').innerHTML < 50) {
+    document.getElementById('li2').style.color = "rgb(18,163,0)";
+} else if (document.getElementById('li2').innerHTML < 100) {
+    document.getElementById('li2').style.color = "rgb(235,240,2)";
+} else if (document.getElementById('li2').innerHTML < 150) {
+    document.getElementById('li2').style.color = "rgb(239,133,0)";
+} else if (document.getElementById('li2').innerHTML < 200) {
+    document.getElementById('li2').style.color = "rgb(251,34,35)";
+} else if (document.getElementById('li2').innerHTML < 300) {
+    document.getElementById('li2').style.color = "rgb(122,49,195)";
+} else if (document.getElementById('li2').innerHTML > 300) {
+    document.getElementById('li2').style.color = "rgb(127,46,70)";
+}
